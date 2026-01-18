21ffed7f7455a234925cf10935ce79bc65d66522
Thiago Matheus <thiagomatheus2028@gmail.com>
2026-01-18 02:39:05 -0300

    chore: initial Spring Boot setup with SB Admin 2

---
+++ src/main/java/br/net/veritylabs/VeritylabsApplication.java
@@ -0,0 +1,13 @@
+package br.net.veritylabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeritylabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeritylabsApplication.class, args);
+	}
+
+}
