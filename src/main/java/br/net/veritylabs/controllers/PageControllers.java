c7f0077d058b471d4650a2a435460118559ff85e
Thiago Matheus <thiagomatheus2028@gmail.com>
2026-01-18 14:16:59 -0300

    chore: setup base Thymeleaf layout and dashboard route

---
+++ src/main/java/br/net/veritylabs/controllers/PageControllers.java
@@ -0,0 +1,15 @@
+package br.net.veritylabs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageControllers {
+
+    @GetMapping({"/", "/dashboard"})
+    public String dashboard(Model model) {
+        model.addAttribute("title", "Dashboard - VerityLabs");
+        return "pages/dashboard";
+    }
+}
